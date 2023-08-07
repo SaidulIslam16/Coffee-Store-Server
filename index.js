d53796aecd0b9eafbd148e0599fb50cded85f8a3
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-08 01:04:17 +0600

    delete oparation added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -34,10 +34,19 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/coffee/:id')
+
         app.post('/coffee', async (req, res) => {
             const newCoffee = req.body;
             console.log(newCoffee);
             const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.deleteOne(query);
             res.send(result);
         })
         // Send a ping to confirm a successful connection
